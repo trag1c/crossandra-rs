a4375f8a639a15610d642638de0ced7a49f9991e
trag1c <trag1cdev@yahoo.com>
2024-09-30 17:29:01 +0200

    refactor: move tree functionality to its own module

--- src/lib.rs
+++ src/lib.rs
@@ -10,13 +10,10 @@ use regex::Regex;
 mod error;
 use error::Error;
 
-const WHITESPACE: [char; 6] = [' ', '\x0c', '\t', '\x0b', '\r', '\n'];
+mod tree;
+use tree::{generate_tree, Tree};
 
-#[derive(Debug, Clone)]
-enum Tree {
-    Leaf(String),
-    Node(HashMap<Option<char>, Tree>),
-}
+const WHITESPACE: [char; 6] = [' ', '\x0c', '\t', '\x0b', '\r', '\n'];
 
 #[derive(Debug)]
 pub struct Token {
@@ -311,39 +308,6 @@ impl<'a> Default for Crossandra<'a> {
     fn default() -> Self {
         Self::new(HashMap::new(), Vec::new(), Vec::new(), true, false, false).unwrap()
     }
-}
-
-fn generate_tree(literals: &HashMap<&str, &str>) -> Tree {
-    let mut sorted_items: Vec<_> = literals.iter().collect();
-    sorted_items.sort_by(|(k1, _), (k2, _)| k2.len().cmp(&k1.len()));
-
-    let mut root = Tree::Node(HashMap::new());
-
-    for (k, v) in sorted_items {
-        let mut current = &mut root;
-
-        for c in k[..k.len() - 1].chars() {
-            if let Tree::Node(ref mut map) = current {
-                current = map.entry(Some(c)).or_insert(Tree::Node(HashMap::new()));
-            }
-        }
-
-        if let Tree::Node(map) = current {
-            let last_char = k.chars().last().expect("Key should not be empty");
-            let token_name = Tree::Leaf((*v).to_string());
-            match map.get_mut(&Some(last_char)) {
-                Some(inner_tree) => {
-                    if let Tree::Node(node) = inner_tree {
-                        node.insert(None, token_name);
-                    }
-                }
-                None => {
-                    map.insert(Some(last_char), token_name);
-                }
-            };
-        }
-    }
-    root
 }
 
 fn flip_hashmap<'a>(hm: HashMap<&'a str, &'a str>) -> HashMap<&'a str, &'a str> {
---
+++ src/tree.rs
@@ -0,0 +1,40 @@
+use std::collections::HashMap;
+
+#[derive(Debug, Clone)]
+pub(crate) enum Tree {
+    Leaf(String),
+    Node(HashMap<Option<char>, Tree>),
+}
+
+pub(crate) fn generate_tree(literals: &HashMap<&str, &str>) -> Tree {
+    let mut sorted_items: Vec<_> = literals.iter().collect();
+    sorted_items.sort_by(|(k1, _), (k2, _)| k2.len().cmp(&k1.len()));
+
+    let mut root = Tree::Node(HashMap::new());
+
+    for (k, v) in sorted_items {
+        let mut current = &mut root;
+
+        for c in k[..k.len() - 1].chars() {
+            if let Tree::Node(ref mut map) = current {
+                current = map.entry(Some(c)).or_insert(Tree::Node(HashMap::new()));
+            }
+        }
+
+        if let Tree::Node(map) = current {
+            let last_char = k.chars().last().expect("Key should not be empty");
+            let token_name = Tree::Leaf((*v).to_string());
+            match map.get_mut(&Some(last_char)) {
+                Some(inner_tree) => {
+                    if let Tree::Node(node) = inner_tree {
+                        node.insert(None, token_name);
+                    }
+                }
+                None => {
+                    map.insert(Some(last_char), token_name);
+                }
+            };
+        }
+    }
+    root
+}
