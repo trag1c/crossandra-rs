e67b4be584d85c93eb9def26bbb89113e1bc2e38
trag1c <trag1cdev@yahoo.com>
2024-10-15 22:30:59 +0200

    test: add error tests

--- src/error.rs
+++ src/error.rs
@@ -16,3 +16,22 @@ impl std::fmt::Display for Error {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::error::Error;
+
+    #[test]
+    fn error_diplay() {
+        assert_eq!(Error::BadToken('x').to_string(), "invalid token 'x'");
+        assert_eq!(
+            Error::DuplicatePattern("string".into()).to_string(),
+            "duplicate pattern \"string\""
+        );
+        assert_eq!(Error::EmptyLiteral.to_string(), "literals cannot be empty");
+        assert_eq!(
+            Error::InvalidRegex(Box::new(fancy_regex::Regex::new("+").unwrap_err())).to_string(),
+            "Parsing error at position 0: Target of repeat operator is invalid"
+        );
+    }
+}
