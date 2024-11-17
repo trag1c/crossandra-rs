d23f09dd98cbb00b7594a4275bacecc3f60cac8f
trag1c <trag1cdev@yahoo.com>
2024-11-17 12:47:22 +0100

    refactor: move Token to its own module

--- src/lib.rs
+++ src/lib.rs
@@ -36,42 +36,15 @@ pub mod common;
 mod error;
 use error::Error;
 
+mod token;
+pub use token::Token;
+
 mod tree;
 use tree::{generate_tree, Tree};
 
 mod patterns;
 
 const WHITESPACE: [char; 6] = [' ', '\x0c', '\t', '\x0b', '\r', '\n'];
-
-/// Represents a lexical token with a name/type and its raw value from the source code.
-///
-/// Used to represent the output of the [`Tokenizer`] struct.
-///
-/// # Examples
-/// ```
-/// # use crossandra::Token;
-/// let num = Token { name: "int".into(), value: "23".into() };
-/// let kw = Token::from(("keyword", "if"));
-/// assert_eq!(num, Token::from(("int", "23")));
-/// # assert_eq!(kw, Token { name: "keyword".into(), value: "if".into() });
-/// # assert_eq!(format!("{num:?}"), "Token { name: \"int\", value: \"23\" }");
-/// ```
-#[derive(Debug, PartialEq, Eq)]
-pub struct Token {
-    /// The type or category of the token (e.g., "int", "identifier", "operator").
-    pub name: String,
-    /// The actual text/value from the source code that this token represents.
-    pub value: String,
-}
-
-impl<T: Into<String>> From<(T, T)> for Token {
-    fn from(value: (T, T)) -> Self {
-        Token {
-            name: value.0.into(),
-            value: value.1.into(),
-        }
-    }
-}
 
 /// The Crossandra tokenizer, operating on literals and patterns.
 ///
---
+++ src/token.rs
@@ -0,0 +1,29 @@
+/// Represents a lexical token with a name/type and its raw value from the source code.
+///
+/// Used to represent the output of the [`Tokenizer`] struct.
+///
+/// # Examples
+/// ```
+/// # use crossandra::Token;
+/// let num = Token { name: "int".into(), value: "23".into() };
+/// let kw = Token::from(("keyword", "if"));
+/// assert_eq!(num, Token::from(("int", "23")));
+/// # assert_eq!(kw, Token { name: "keyword".into(), value: "if".into() });
+/// # assert_eq!(format!("{num:?}"), "Token { name: \"int\", value: \"23\" }");
+/// ```
+#[derive(Debug, PartialEq, Eq)]
+pub struct Token {
+    /// The type or category of the token (e.g., "int", "identifier", "operator").
+    pub name: String,
+    /// The actual text/value from the source code that this token represents.
+    pub value: String,
+}
+
+impl<T: Into<String>> From<(T, T)> for Token {
+    fn from(value: (T, T)) -> Self {
+        Token {
+            name: value.0.into(),
+            value: value.1.into(),
+        }
+    }
+}
