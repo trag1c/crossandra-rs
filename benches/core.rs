c8af68446943579c40a5130e932c7f5e8c750557
Maneren <maneren731@gmail.com>
2024-12-12 01:37:40 +0100

    feat: added simple benchmark suite

---
+++ benches/core.rs
@@ -0,0 +1,125 @@
+use rustc_hash::FxHashMap;
+
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use crossandra::{common, Tokenizer};
+
+pub fn samarium(c: &mut Criterion) {
+    let literals = [
+        ("ad", "+"),
+        ("an", "&&"),
+        ("ar", "@@@"),
+        ("as", ":"),
+        ("at", "."),
+        ("ba", "&"),
+        ("bn", "~"),
+        ("bo", "|"),
+        ("brace_c", "}"),
+        ("brace_o", "{"),
+        ("brack_c", "]"),
+        ("brack_o", "["),
+        ("bx", "^"),
+        ("cas", "%"),
+        ("cat", "!!"),
+        ("cl", "@"),
+        ("da", "@!"),
+        ("de", "<>"),
+        ("di", "--"),
+        ("e", ",,"),
+        ("end", ";"),
+        ("ent", "=>"),
+        ("enu", "#"),
+        ("eq", "::"),
+        ("ex", "=>!"),
+        ("fi_a", "&~~>"),
+        ("fi_b_a", "&%~>"),
+        ("fi_b_r", "<~%"),
+        ("fi_b_r_w", "<%>"),
+        ("fi_b_w", "%~>"),
+        ("fi_c", "?~>"),
+        ("fi_q_a", "&~>"),
+        ("fi_q_b_a", "&%>"),
+        ("fi_q_b_r", "<%"),
+        ("fi_q_b_w", "%>"),
+        ("fi_q_r", "<~"),
+        ("fi_q_w", "~>"),
+        ("fi_r_w", "<~>"),
+        ("fi_r", "<~~"),
+        ("fi_w", "~~>"),
+        ("fo", "..."),
+        ("fr", "<-"),
+        ("fu", "*"),
+        ("ge", ">:"),
+        ("gt", ">"),
+        ("h", "##"),
+        ("if", "?"),
+        ("im", "<="),
+        ("in", "->?"),
+        ("ins", "'"),
+        ("le", "<:"),
+        ("lt", "<"),
+        ("mo", "---"),
+        ("mu", "++"),
+        ("ne", ":::"),
+        ("no", "~~"),
+        ("o", "||"),
+        ("p_c", ")"),
+        ("p_o", "("),
+        ("pa", "!?"),
+        ("po", "+++"),
+        ("pr", "!"),
+        ("r", "???"),
+        ("se", ","),
+        ("sle", ",.,"),
+        ("s_c", ">>"),
+        ("s_o", "<<"),
+        ("sp", "$"),
+        ("su", "-"),
+        ("t_c", "}}"),
+        ("t_o", "{{"),
+        ("th", "!!!"),
+        ("to", "->"),
+        ("tr", "??"),
+        ("ty", "?!"),
+        ("u", "@@"),
+        ("w", ".."),
+        ("x", "^^"),
+        ("y", "**"),
+        ("z", "><"),
+    ];
+    let patterns = [
+        common::DOUBLE_QUOTED_STRING.clone(),
+        ("number".into(), r"[\\/]+`?[\\/]*|`[\\/]*".into()),
+        ("block_comment".into(), r"==<.*>==".into()),
+        ("line_comment".into(), r"==[^\n]*".into()),
+        ("variable".into(), r"\w+".into()),
+    ];
+    let literals = FxHashMap::from_iter(literals);
+    let tok = Tokenizer::default()
+        .with_literals(&literals)
+        .unwrap()
+        .with_patterns(patterns.into())
+        .unwrap()
+        .with_ignore_whitespace(true);
+
+    let datetime = include_str!("assets/samarium/datetime.sm");
+
+    c.bench_function("samarium datetime.sm", |b| {
+        b.iter(|| {
+            black_box(
+                tok.tokenize(black_box(datetime))
+                    .collect::<Result<Vec<_>, _>>(),
+            )
+        })
+    });
+
+    let mut long_group = c.benchmark_group("samarium long");
+    long_group.sample_size(10);
+    let long = include_str!("assets/samarium/long.sm");
+
+    long_group.bench_function("samarium long.sm", |b| {
+        b.iter(|| black_box(tok.tokenize(black_box(long)).collect::<Result<Vec<_>, _>>()))
+    });
+}
+
+criterion_group!(benches, samarium);
+criterion_main!(benches);
---
+++ benches/fast.rs
@@ -0,0 +1,73 @@
+use rustc_hash::FxHashMap;
+
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use crossandra::Tokenizer;
+
+pub fn brainfuck(c: &mut Criterion) {
+    let tok = Tokenizer::default()
+        .with_literals(&FxHashMap::from_iter([
+            ("add", "+"),
+            ("sub", "-"),
+            ("left", "<"),
+            ("right", ">"),
+            ("read", ","),
+            ("write", "."),
+            ("begin_loop", "["),
+            ("end_loop", "]"),
+        ]))
+        .unwrap();
+
+    // 100 chars-long program that prints "Hello World!"
+    let hello_world = include_str!("assets/brainfuck/hello_world.bf");
+
+    c.bench_function("brainfuck hello_world", |b| {
+        b.iter(|| {
+            black_box(
+                tok.tokenize(black_box(hello_world))
+                    .collect::<Result<Vec<_>, _>>(),
+            )
+        })
+    });
+
+    // 12k chars-long program that visualizes the mandelbrot set
+    let mandelbrot = include_str!("assets/brainfuck/mandelbrot.bf");
+
+    c.bench_function("brainfuck mandelbrot", |b| {
+        b.iter(|| {
+            black_box(
+                tok.tokenize(black_box(mandelbrot))
+                    .collect::<Result<Vec<_>, _>>(),
+            )
+        })
+    });
+
+    // 50k chars-long program that solves the hanoi towers problem
+    let hanoi = include_str!("assets/brainfuck/hanoi.bf");
+
+    c.bench_function("brainfuck hanoi", |b| {
+        b.iter(|| {
+            black_box(
+                tok.tokenize(black_box(hanoi))
+                    .collect::<Result<Vec<_>, _>>(),
+            )
+        })
+    });
+
+    let mut huge_group = c.benchmark_group("brainfuck huge");
+    huge_group.sample_size(30);
+    // 1M chars-long file with random characters (mostly brainfuck with some comments)
+    let huge = include_str!("assets/brainfuck/huge.bf");
+
+    huge_group.bench_function("brainfuck huge", |b| {
+        b.iter(|| {
+            black_box(
+                tok.tokenize(black_box(huge))
+                    .filter_map(Result::ok)
+                    .collect::<Vec<_>>(),
+            )
+        })
+    });
+}
+
+criterion_group!(benches, brainfuck);
+criterion_main!(benches);
