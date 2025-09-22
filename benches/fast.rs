0b2d207f589d720de23eb8227d8f05b53c0ebbb9
Maneren <maneren731@gmail.com>
2025-09-22 22:47:14 +0200

    feat: update dependencies
    The version of fancy-regex speeds up the benchmarks by up to 500x

--- benches/fast.rs
+++ benches/fast.rs
@@ -1,4 +1,6 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use std::hint::black_box;
+
+use criterion::{criterion_group, criterion_main, Criterion};
 use crossandra::Tokenizer;
 
 pub fn brainfuck(c: &mut Criterion) {
